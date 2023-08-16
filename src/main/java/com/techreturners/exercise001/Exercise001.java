294bda8521e64ef949e4dca77da9cf7c7cdceae0
Ade Yates <adeyatz@gmailcom>
2023-08-16 15:59:43 +0100

    feat: Exercise001 - first imp. of capitalizeWord

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+    		return word.substring(0,1).toUpperCase().concat(word.substring(1));
     }
 
     public String generateInitials(String firstName, String lastName) {
