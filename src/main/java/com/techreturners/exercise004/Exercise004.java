cae1a85739b5ae716084c8725bc2471d1dbfeb9b
Ade Yates <adeyatz@gmailcom>
2023-08-17 16:46:47 +0100

    feat(Exercise004): First implementation, pass 3 tests

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -4,16 +4,28 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Exercise004 {
+	
+	private LocalDateTime dateTime;
+	private final long oneGigaSecond = 1000000000;
+
 
     public Exercise004(LocalDate date) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.dateTime = date.atStartOfDay();
+        addTimeInSeconds(oneGigaSecond);
     }
 
     public Exercise004(LocalDateTime dateTime) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.dateTime = dateTime;
+        addTimeInSeconds(oneGigaSecond);
+    }
+    
+    
+    public void addTimeInSeconds (long secondsToAdd) {
+    	dateTime = dateTime.plusSeconds(secondsToAdd);
     }
 
+    
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return dateTime;
     }
 }
